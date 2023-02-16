52cf4795174976b6f429e93b51431a66dc0f13f9
lyl <1106225065@qq.com>
2023-02-16 22:01:34 +0800

    feat: axios拦截器统一进行权限判断

--- src/service/errorHandle.js
+++ src/service/errorHandle.js
@@ -1,7 +1,11 @@
 // 错误统一处理
-const errorHandle = () => {
-  console.log('errhandle');
-  window.location.href = window.location.origin + '/error';
+const errorHandle = (error) => {
+  if (error.response.status === 401) { // 身份过期/token无效
+    //  跳转到登录页面登录
+    setTimeout(() => {
+      window.location.href = window.location.origin + '/login';
+    }, 200)
+  }
 }
 
 export default errorHandle;
